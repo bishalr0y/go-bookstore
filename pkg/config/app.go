c26477f5e76aaa55ec2da7e8105ebc4ec706d28b
Bishal Roy <bishalroy895@gmail.com>
2024-03-27 15:55:46 +0530

    feat: add error handling for db

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,21 +1,14 @@
 package config
 
 import (
-	"fmt"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func ConnectDb() *gorm.DB {
+func ConnectDb() (*gorm.DB, error) {
 
 	dsn := "host=localhost user=postgres password=postgres dbname=bookstore port=5430 sslmode=disable TimeZone=Asia/Kolkata"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Database connected")
-	return db
+	return db, err
 
 }
