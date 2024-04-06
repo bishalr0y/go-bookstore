087c30a3217c8417cd6e40fa0ebd750eecb5911d
Bishal Roy <bishalroy895@gmail.com>
2024-04-06 22:17:32 +0530

    feat: create book and get book API

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 
 	"github.com/bishalr0y/go-bookstore/pkg/config"
+	"github.com/bishalr0y/go-bookstore/pkg/models"
+	"github.com/bishalr0y/go-bookstore/pkg/routers"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Welcome to Go Bookstore")
-	db, err := config.ConnectDb()
+	fmt.Println("Welcome to Go Bookstore!")
+	r := gin.Default()
+	db := config.ConnectDb()
 
-	if err != nil {
-		panic("Error connecting to the database!")
-	}
+	models.Init()
+
 	// do something with the database
 	fmt.Println(db)
+
+	routers.RegisterBookstoreRoutes(r)
+
+	r.Run(":9010")
+
 }
+
+//! Calling the router and running the server on the port 9010
