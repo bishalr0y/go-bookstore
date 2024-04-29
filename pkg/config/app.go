2195601f93b2f9a665a3c9989596766ad8763e8b
Bishal Roy <bishalroy895@gmail.com>
2024-04-29 21:50:57 +0530

    feat: add docker compose

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,16 +1,21 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDb() *gorm.DB {
 
-	dsn := "host=localhost user=postgres password=postgres dbname=bookstore port=5430 sslmode=disable TimeZone=Asia/Kolkata"
+	dsn := "host=postgres-bookstore user=postgres password=postgres dbname=bookstore port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	// dsn := "host=localhost user=postgres password=postgres dbname=bookstore port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
+		fmt.Println(err.Error())
 		panic("Error connecting to the database!")
 	}
 	return db
